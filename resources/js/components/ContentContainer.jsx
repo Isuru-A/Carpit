463dd80aeec96477828c6ddb2ba27d48799f810c
isuru <isuru2003a@gmail.com>
2023-09-06 23:19:32 +1000

    Make services content container

--- resources/js/components/ContentContainer.jsx
+++ resources/js/components/ContentContainer.jsx
@@ -2,18 +2,21 @@ import {motion} from "framer-motion";
 import logo from "../../../public/assets/logo.png";
 import NavLink from "../elements/NavLink.jsx";
 
-const ContentContainer = ({width, children}) => {
+const ContentContainer = ({id, width, children}) => {
 
     return (
         <div className="content-wrapper">
             <motion.div className="content-container"
+                        key={id}
                         initial={{width: 0}}
                         animate={{width: `${width}vw`}}
+                        exit={{width: 0}}
             >
                 <img src={logo} alt="CARPIT"/>
                 <div className="content">
                     {children}
                 </div>
+
             </motion.div>
             <motion.nav className="vert-navigation"
                         initial={{opacity: 0}}
--- resources/js/components/Services/Services.jsx
+++ resources/js/components/Services/Services.jsx
@@ -1,7 +1,19 @@
+import ContentContainer from "../ContentContainer.jsx";
+import {NavLink} from "react-router-dom";
+
 const Services = () => {
 
     return (
-
+        <ContentContainer width={20}>
+            <h1>Services</h1>
+            <nav className="service-navigation">
+                <NavLink to="/services/servicing">Servicing</NavLink>
+                <NavLink to="/services/transmission">Transmission</NavLink>
+                <NavLink to="/services/cleaning">Cleaning</NavLink>
+                <NavLink to="/services/detailing" id="enquire-now">Detailing</NavLink>
+                <NavLink to="/services/tuning" id="enquire-now">Tuning</NavLink>
+            </nav>
+        </ContentContainer>
     )
 }
 
