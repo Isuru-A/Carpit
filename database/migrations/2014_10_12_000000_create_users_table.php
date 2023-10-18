1b1c842a63354526a8db5c2040dbaa172ef337b9
isuru <isuru2003a@gmail.com>
2023-10-18 18:08:50 +1100

    Make user phone nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name_first');
             $table->string('name_last');
             $table->string('email')->unique();
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('admin')->default(0);
