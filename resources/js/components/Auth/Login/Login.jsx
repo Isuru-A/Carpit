3a1baaef622dc857edd39d6a0cd0ec9660f9829b
isuru <isuru2003a@gmail.com>
2023-10-18 12:47:58 +1100

    Post to login endpoint and handle server errors

--- resources/js/components/Auth/Login/Login.jsx
+++ resources/js/components/Auth/Login/Login.jsx
@@ -5,8 +5,11 @@ import Form from "../../../elements/Form.jsx";
 import Field from "../../../elements/Field.jsx";
 import validate from "../../../../scripts/validate.js";
 import InLineButton from "../../../elements/InLineButton.jsx";
+import {useState} from "react";
 
 const Login = () => {
+
+    const [serverErrors, setServerErrors] = useState({})
 
     return (
         <FadeInDiv id="login-wrapper">
@@ -14,14 +17,20 @@ const Login = () => {
                 <Formik initialValues={{
                     email: '',
                     password: ''
-                }} onSubmit={async () => {
-
+                }} onSubmit={async (values) => {
+                    await axios.post('/auth/login', values)
+                        .then(response => {
+                            console.log(response.data)
+                        })
+                        .catch(e => {
+                            setServerErrors(e.response.data.errors)
+                        })
                 }}>
                     {({errors}) => (
                         <>
                             <Form id="auth-form" cta="Enter">
-                                <Field name="email" label="Email" validate={validate.email} error={errors.email}/>
-                                <Field password name="password" label="Password" validate={validate.require} error={errors.password}/>
+                                <Field name="email" label="Email" validate={validate.email} error={serverErrors.email ? serverErrors.email : errors.email}/>
+                                <Field password name="password" label="Password" validate={validate.require} error={serverErrors.password ? serverErrors.password : errors.password}/>
                             </Form>
                             <div className="auth-actions">
                                 <InLineButton>Forgot Password?</InLineButton>
