22ce6c028060e21596b5b031e3107091b9b4033f
isuru <isuru2003a@gmail.com>
2023-10-24 19:33:50 +1100

    Add repository method to get all messages linked to an enquiry

--- app/Contracts/Repository/MessageRepositoryInterface.php
+++ app/Contracts/Repository/MessageRepositoryInterface.php
@@ -2,7 +2,15 @@
 
 namespace App\Contracts\Repository;
 
+use Illuminate\Database\Eloquent\Collection;
+
 interface MessageRepositoryInterface extends RepositoryInterface
 {
-
+    /**
+     * Get all the messages associated with an enquiry by ID
+     *
+     * @param int $enquiryId
+     * @return Collection
+     */
+    public function getEnquiryMessages(int $enquiryId): Collection;
 }
--- app/Repositories/MessageRepository.php
+++ app/Repositories/MessageRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Contracts\Repository\MessageRepositoryInterface;
 use App\Models\Message;
+use Illuminate\Database\Eloquent\Collection;
 
 class MessageRepository extends Repository implements MessageRepositoryInterface
 {
@@ -11,5 +12,10 @@ class MessageRepository extends Repository implements MessageRepositoryInterface
     public function model(): string
     {
         return Message::class;
+    }
+
+    public function getEnquiryMessages(int $enquiryId): Collection
+    {
+        return $this->getBuilder()->where('enquiry_id', $enquiryId)->get();
     }
 }
