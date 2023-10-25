fa17b1c81dfe9dd06d664f5969fb67699ff3d617
isuru <isuru2003a@gmail.com>
2023-10-25 17:50:37 +1100

    Create controller and route to mark enquiry as active

--- app/Http/Controllers/Admin/AdminEnquiryController.php
+++ app/Http/Controllers/Admin/AdminEnquiryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Contracts\Repository\EnquiryRepositoryInterface;
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -30,5 +31,29 @@ class AdminEnquiryController extends Controller
     public function get(Request $request, $id)
     {
         return $this->enquiryRepository->get($id);
+    }
+
+    /**
+     * Mark enquiry as active. Used for enquiries without a registered user
+     *
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     */
+    public function accept(Request $request, $id): JsonResponse
+    {
+        try {
+            $this->enquiryRepository->update($id, [
+                'active' => 1
+            ]);
+        } catch (Exception $e) {
+            return new JsonResponse([
+                'success' => false
+            ], 500);
+        }
+
+        return new JsonResponse([
+            'success' => true
+        ]);
     }
 }
--- routes/api/admin.php
+++ routes/api/admin.php
@@ -17,5 +17,6 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
     Route::prefix('enquiries')->group(function () {
         Route::get('/', [\App\Http\Controllers\Admin\AdminEnquiryController::class, 'index']);
         Route::get('/{id}', [\App\Http\Controllers\Admin\AdminEnquiryController::class, 'get']);
+        Route::post('/{id}/accept', [\App\Http\Controllers\Admin\AdminEnquiryController::class, 'accept']);
     });
 });
