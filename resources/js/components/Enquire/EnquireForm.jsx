fe0a52489a80cb9ffd33f59bf9b18eb24bd181a4
isuru <isuru2003a@gmail.com>
2023-10-26 22:55:41 +1100

    Add not logged in message

--- resources/js/components/Enquire/EnquireForm.jsx
+++ resources/js/components/Enquire/EnquireForm.jsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import EnquireFormName from "./EnquireFormName";
 import EnquireFormEnquiry from "./EnquireFormEnquiry";
 import EnquirySent from "./EnquirySent.jsx";
+import MessageBox from "../../elements/MessageBox.jsx";
 
 const EnquireForm = () => {
 
@@ -22,6 +23,14 @@ const EnquireForm = () => {
         <>
             <h1 id="enquire-heading">Enquire Now</h1>
             {views[state]}
+            <MessageBox id="enquire-message-box" weight="warning">
+                You are not logged in!
+                <br/><br/>
+                Logging in will enable you to chat with our mechanics <br/> securely using the CarPit app and track the
+                progress <br/> of your service.
+                <br/><br/>
+                Without an account, our mechanics will only be able <br/> to contact you via email and/or text
+            </MessageBox>
         </>
     )
 }
