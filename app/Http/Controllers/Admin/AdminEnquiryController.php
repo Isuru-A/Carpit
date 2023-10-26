cd4ad1613bf3d4ef9c09c3ab2d021ede536ab8b0
isuru <isuru2003a@gmail.com>
2023-10-26 23:39:27 +1100

    Create controller method and route to archive enquiries

--- app/Http/Controllers/Admin/AdminEnquiryController.php
+++ app/Http/Controllers/Admin/AdminEnquiryController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class AdminEnquiryController extends Controller
 {
@@ -50,6 +49,32 @@ class AdminEnquiryController extends Controller
             return new JsonResponse([
                 'success' => false
             ], 500);
+        }
+
+        return new JsonResponse([
+            'success' => true
+        ]);
+    }
+
+    /**
+     * Handle request to archive an enquiry
+     *
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     */
+    public function archive(Request $request, $id): JsonResponse
+    {
+        try {
+            $this->enquiryRepository->update($id, [
+                'active' => 0,
+                'archived' => 1,
+
+            ]);
+        } catch (Exception $e) {
+            return new JsonResponse([
+                'success' => false
+            ]);
         }
 
         return new JsonResponse([
--- routes/api/admin.php
+++ routes/api/admin.php
@@ -18,5 +18,6 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
         Route::get('/', [\App\Http\Controllers\Admin\AdminEnquiryController::class, 'index']);
         Route::get('/{id}', [\App\Http\Controllers\Admin\AdminEnquiryController::class, 'get']);
         Route::post('/{id}/accept', [\App\Http\Controllers\Admin\AdminEnquiryController::class, 'accept']);
+        Route::post('/{id}/archive', [App\Http\Controllers\Admin\AdminEnquiryController::class, 'archive']);
     });
 });
