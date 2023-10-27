2d98cb3dbb668ea4b1588a9827155b855cce8a07
isuru <isuru2003a@gmail.com>
2023-10-27 21:28:34 +1100

    Add optional id to Select

--- resources/js/elements/Select.jsx
+++ resources/js/elements/Select.jsx
@@ -1,13 +1,13 @@
 import {Field as FormikField} from "formik";
 
-const Select = ({name, label, error, children}) => {
+const Select = ({name, label, error, children, id}) => {
 
     return (
         <FormikField name={name}>
             {({field: {value}, form: {setFieldValue}}) => (
                 <div className="field-wrapper">
                     <span className="field-label">{label}</span>
-                    <select id={name} className="field" defaultValue={value} onChange={(e) => {
+                    <select id={id ? id :  name} className="field" defaultValue={value} onChange={(e) => {
                         setFieldValue(name, e.target.value)
                     }}>
                         {children}
