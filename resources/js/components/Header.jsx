881a5656e98e0e74c649811a40e45ecf5913f5ba
isuru <isuru2003a@gmail.com>
2023-10-27 22:02:21 +1100

    Add login button

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -1,12 +1,19 @@
 import Navigation from "./Navigation.jsx";
 import logo from "../../../public/assets/logo.png"
+import InLineButton from "../elements/InLineButton.jsx";
+import {useNavigate} from "react-router-dom";
 
 const Header = () => {
+
+    const navigate = useNavigate()
 
     return (
         <div id="header">
             <img src={logo} alt="CARPIT"/>
             <Navigation/>
+            <InLineButton id="login-button" onClick={() => {
+                navigate('/auth/login')
+            }}>Login</InLineButton>
         </div>
     )
 }
