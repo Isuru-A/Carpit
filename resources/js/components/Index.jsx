3616f740cfdb7b5ff9bd4e385ac5f2b3fe0c6076
isuru <isuru2003a@gmail.com>
2023-11-24 17:44:54 +1100

    add mobile block

--- resources/js/components/Index.jsx
+++ resources/js/components/Index.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import Router from "./Router.jsx";
+import {MobileView, BrowserView} from "react-device-detect";
 
 function Index() {
     return (
         <div className="app">
-            <BrowserRouter>
-                <Router/>
-            </BrowserRouter>
+            <BrowserView>
+                <BrowserRouter>
+                    <Router/>
+                </BrowserRouter>
+            </BrowserView>
+            <MobileView>
+                <div className="mobile-block">
+                    <h1>Carpit Automotive is not available on mobile yet</h1>
+                    <span>Please visit on a desktop browser</span>
+                </div>
+            </MobileView>
         </div>
     );
 }
